1480fd29f0b457c9f04f928f2ba1450270076f7e
moses877891 <mosesantonymas@gmail.com>
2022-11-16 16:17:57 +0530

    added deletion and grouping in toDo list

--- src/components/todo-table/todo-table.component.jsx
+++ src/components/todo-table/todo-table.component.jsx
@@ -1,35 +1,81 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 import { TodoContext } from "../../context/todo.context";
 
 import ToDoListComponent from "../todo-list/todo-list.component";
 
 const TodoTable = () => {
-    const { todoList } = useContext(TodoContext);
-    // const [order, setOrder] = useState("ASC");
+    const { todoList, setTodoList } = useContext(TodoContext);
+    const [orderToDo, setOrderToDO] = useState("ASC");
+    const [orderDate, setOrderDate] = useState("ASC");
 
-    // const sorting = (col) => {
-    //     if (order === 'ASC') {
-    //         const sorted = [...todoList].sort((a, b) =>
-    //             //console.log(a.col);
-    //             a[col].toLowercase() > b[col].toLowercase() ? 1 : -1
+    const sortingToDo = () => {
+        if (orderToDo === 'ASC') {
+            const sorted = [...todoList].sort((a, b) =>
 
-    //         );
+                a.toDo.toString().toLowerCase() > b.toDo.toString().toLowerCase() ? 1 : -1
 
-    //         setTodoList(sorted);
-    //         setOrder("DSC");
-    //     }
-    // }
+            );
+
+            setTodoList(sorted);
+            setOrderToDO("DSC");
+        }
+        if (orderToDo === 'DSC') {
+            const sorted = [...todoList].sort((a, b) =>
+
+                a.toDo.toString().toLowerCase() < b.toDo.toString().toLowerCase() ? 1 : -1
+
+            );
+
+            setTodoList(sorted);
+            setOrderToDO("ASC");
+        }
+    }
+
+    const sortingDate = () => {
+        if (orderDate === 'ASC') {
+            const sorted = [...todoList].sort((a, b) =>
+
+                a.date > b.date ? 1 : -1
+
+            );
+
+            setTodoList(sorted);
+            setOrderDate("DSC");
+        }
+        if (orderDate === 'DSC') {
+            const sorted = [...todoList].sort((a, b) =>
+
+                a.date < b.date ? 1 : -1
+
+            );
+
+            setTodoList(sorted);
+            setOrderDate("ASC");
+        }
+    }
 
     return (
         <div>
             <table className="w-full text-sm text-left text-gray-700 dark:text-gray-400">
                 <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700'>
                     <tr>
-                        <th className='py-3 px-6'>toDo</th>
+                        <th className='py-3 px-6 cursor-pointer' onClick={() => sortingToDo()}>toDo
+                            {
+                                orderToDo === 'ASC' ?
+                                    (<span className=" mx-2 px-1 bg-gray-300 rounded">ASC</span>) : (
+                                        <span className="mx-2 px-1 bg-gray-300 rounded">DSC</span>)
+                            }
+                        </th>
                         <th className='py-3 px-6'>note</th>
                         <th className='py-3 px-6'>priority</th>
-                        <th className='py-3 px-6'>date</th>
+                        <th className='py-3 px-6 cursor-pointer' onClick={() => sortingDate()}>date
+                            {
+                                orderDate === 'ASC' ?
+                                    (<span className=" mx-2 px-1 bg-gray-300 rounded">ASC</span>) : (
+                                        <span className="mx-2 px-1 bg-gray-300 rounded">DSC</span>)
+                            }
+                        </th>
                         <th className='py-3 px-6'></th>
                     </tr>
                 </thead>
